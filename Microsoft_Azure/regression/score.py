bbd5d77585961286ba9adb4a82daf54a282fbbed
SkylerSG <132885096+SkylerSG@users.noreply.github.com>
2023-08-09 05:19:08 +1200

    Regression model s1
    not currently working, added .pkl files to gitignore as they're larger than 100mb

---
+++ Microsoft_Azure/regression/score.py
@@ -0,0 +1,24 @@
+import json, os, joblib
+import numpy as np
+
+# Since the model works with label-encoded data, we can create a dictionary to get the actual class names
+classes = {0: "setosa", 1: "versicolor", 2: "virginica"}
+
+def init():
+    # Loads the model
+    global model
+    model_name = "reg_model.pkl"
+    # AZURE_MODEL_DIR is an Azure environment variable where scripts are stored in the cloud and should not be changed
+    model_path = os.path.join(os.getenv("AZUREML_MODEL_DIR"), model_name)
+    model = joblib.load(model_path)
+
+def run(request):
+    # Loads the input data, runs the model on it, and returns its predictions
+    data = json.loads(request)
+    print(f'Request:{request} completed loading')
+    data = np.array(data["data"])
+    print(f'Data array: {data} completed')
+    result = model.predict(data)
+    print(f'Result: {result}')
+    #return [classes.get(key) for key in result] 
+    return result #Don't need classes for regression model :D
